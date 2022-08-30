b7cbd4b6db442a0629d21e817813468ec4659701
nbenouerkhou <nasreddine.benouerkhou@gmail.com>
2022-08-30 20:38:47 +0200

    animation cards + ajout images page all services

---
+++ animation+ajout-img-cards-all-services/services.php
@@ -0,0 +1,152 @@
+<section class="container container-970 servicepager--Heading text-center">
+    <h2>Découvrez nos services </h2>
+    <p>Nous sommes là pour vous aider à réaliser votre plein potentiel. </p>
+</section>
+
+<section class="service-lists-box">
+    <div class="container container-1160 d-none d-sm-block">
+        <div class="row justify-content-center reset-margin">
+            <div class="service-block col-lg-4 col-md-12 col-sm-12 reset-padding">
+                <div class="service card">
+                    <div class="img-service">
+                        <img src="images/developpement-web.png" alt="image service developpement web" />
+                    </div>
+                    <div class="service-title d-table text-center">
+                        <div class="service-title-innerwrapper d-table-cell align-middle">
+                            <h2>Développement Web</h2>
+                        </div>
+                    </div>
+                    <div class="service-body">
+                        <ul class="service-body--lists">
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Site Vitrine</li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Site E-Commerce</li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Conception UI / UX</li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Application Web</li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Développement Front-End</li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Développement Back-End</li>
+                        </ul>
+                    </div>
+                    <div class="service-footer text-center">
+                        <a href="service_developpement_web.php" class="btn btn-primary btn-c2d-primary-sm">En savoir
+                            plus</a>
+                    </div>
+                </div>
+            </div>
+            <div class="service-block col-lg-4 col-md-12 col-sm-12 reset-padding">
+                <div class="service card">
+                    <div class="img-service">
+                        <img src="images/conception-graphique.png" alt="image service conception graphique" />
+                    </div>
+                    <div class="service-title d-table text-center">
+                        <div class="service-title-innerwrapper d-table-cell align-middle">
+                            <h2>Conception Graphique</h2>
+                        </div>
+                    </div>
+                    <div class="service-body">
+                        <ul class="service-body--lists">
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Visual Identity & Branding Digital
+                            </li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Conception Graphique</li>
+                        </ul>
+                    </div>
+                    <div class="service-footer text-center">
+                        <a href="service_conception_graphique.php" class="btn btn-primary btn-c2d-primary-sm">En savoir
+                            plus</a>
+                    </div>
+                </div>
+            </div>
+            <div class="service-block col-lg-4 col-md-12 col-sm-12 reset-padding">
+                <div class="service card">
+                    <div class="img-service">
+                        <img src="images/cloud&securite.png" alt="image service cloud & securite" />
+                    </div>
+                    <div class="service-title d-table text-center">
+                        <div class="service-title-innerwrapper d-table-cell align-middle">
+                            <h2>Cloud & Sécurité </h2>
+                        </div>
+                    </div>
+                    <div class="service-body">
+                        <ul class="service-body--lists">
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Public Cloud</li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Hybrid Cloud</li>
+                        </ul>
+                    </div>
+                    <div class="service-footer text-center">
+                        <a href="service_cloud_security.php" class="btn btn-primary btn-c2d-primary-sm">En savoir
+                            plus</a>
+                    </div>
+                </div>
+            </div>
+            <div class="service-block col-lg-4 col-md-12 col-sm-12 reset-padding">
+                <div class="service card">
+                    <div class="img-service">
+                        <img src="images/infrastructure.png" alt="image service infrastructure & securite" />
+                    </div>
+                    <div class="service-title d-table text-center">
+                        <div class="service-title-innerwrapper d-table-cell align-middle">
+                            <h2>Infrastructure & Sécurité </h2>
+                        </div>
+                    </div>
+                    <div class="service-body">
+                        <ul class="service-body--lists">
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Network Infrastructure</li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>RTLS</li>
+                        </ul>
+                    </div>
+                    <div class="service-footer text-center">
+                        <a href="service_infrastructure_security.php" class="btn btn-primary btn-c2d-primary-sm">En
+                            savoir plus</a>
+                    </div>
+                </div>
+            </div>
+            <div class="service-block col-lg-4 col-md-12 col-sm-12 reset-padding">
+                <div class="service card">
+                    <div class="img-service">
+                        <img src="images/storage.png" alt="image service Systems, Storage & Virtualisation" />
+                    </div>
+                    <div class="service-title d-table text-center">
+                        <div class="service-title-innerwrapper d-table-cell align-middle">
+                            <h2>Systems, Storage & Virtualisation</h2>
+                        </div>
+                    </div>
+                    <div class="service-body">
+                        <ul class="service-body--lists">
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Infrastructure serveur On-Premise
+                            </li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Stockage des données </li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Virtualisation </li>
+                        </ul>
+                    </div>
+                    <div class="service-footer text-center">
+                        <a href="service_system_storage.php" class="btn btn-primary btn-c2d-primary-sm">En savoir
+                            plus</a>
+                    </div>
+                </div>
+            </div>
+            <div class="service-block col-lg-4 col-md-12 col-sm-12 reset-padding">
+                <div class="service card">
+                    <div class="img-service">
+                        <img src="images/collaboration.png" alt="image service Unified Collaboration & Phone Systems" />
+                    </div>
+                    <div class="service-title d-table text-center">
+                        <div class="service-title-innerwrapper d-table-cell align-middle">
+                            <h2>Unified Collaboration & Phone Systems</h2>
+                        </div>
+                    </div>
+                    <div class="service-body">
+                        <ul class="service-body--lists">
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Visioconférence</li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Outils de collaboration</li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Systèmes Téléphoniques</li>
+                            <li><i class="fa fa-check-circle" aria-hidden="true"></i>Centre d’appels</li>
+                        </ul>
+                    </div>
+                    <div class="service-footer text-center">
+                        <a href="service_unified_collaboration.php" class="btn btn-primary btn-c2d-primary-sm">En savoir
+                            plus</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</setion>
