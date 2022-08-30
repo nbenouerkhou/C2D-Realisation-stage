79e4dccd1194e5d45b38267466912da0090e1b63
nbenouerkhou <nasreddine.benouerkhou@gmail.com>
2022-08-30 21:03:55 +0200

    refonte footer

---
+++ refonte-du-footer/footer.php
@@ -0,0 +1,31 @@
+<footer>
+        <div class="container text-center">
+          <div class="row no-gutters">
+              <div class="col-md-3 footer-link">
+                  <img src="images/logo-c2d.png" alt="logo c2d" width="120px"/>
+
+              </div>
+              <div class="col-md-3 footer-link">
+                  <a class="text-white" href="underconstruction.php"><u >Politique de confidentialité</u></a>
+
+              </div>
+              <div class="col-md-3 footer-link">
+                  <a class="text-white" href="underconstruction.php"><u class="footer-link">Conditions generales d'utilisation</u></a>
+
+              </div>
+              <div class="col-md-3 footer-link">
+                  <a class="text-white" href="underconstruction.php"><u class="footer-link">Notre portefeuille<u></a>
+
+              </div>
+              
+              
+              
+          </div>
+        </div>
+        <div class="separateur"><hr>
+        </div>
+        <div class="copyright">
+           <p><i class="fa fa-copyright" style="font: size 22px;"></i> copyright</p> 
+        </div>
+    
+</footer>
