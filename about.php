4f720c68939da9188640b5cdc17da457f0e553b2
nbenouerkhou <nasreddine.benouerkhou@gmail.com>
2022-08-30 19:15:12 +0200

    refonte de la page qui sommes nous

---
+++ about.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Qui sommes-nous | C2D</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+  <link rel="stylesheet" href="css/mobile_style.css" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/responsive.css" />
+
+  <link rel="icon" href="images/favicon-ico.png" sizes="32x32" />
+  <link rel="icon" href="images/favicon-ico.png" sizes="32x32" />
+  <link rel="apple-touch-icon" href="images/favicon-ico.png" />
+  <meta name="msapplication-TileImage" content="images/favicon-ico.png" />
+
+</head>
+
+<body class="page-about innerPage fr-site">
+  <div class="container-fluid aboutpage-top-fluid reset-padding">
+    <div class="top-sticky-bar text-center">
+      <p>site web en cours de construction, la mise à jour va être sympa :-)</p>
+    </div>
+    <section class="innerpage--banner"
+       style="background-image: url('images/aboutpage-Banner.png'), linear-gradient(180deg, #FBFBFB 0%, rgba(190, 190, 190,0.8) 100%);"> 
+      <?php include 'includes/mobile-menu.php'; ?>
+      <header class="site-header site-navbar overlay-header site-navbar-target">
+        <div class="container container-1280 reset-padding">
+          <?php include 'includes/header.php'; ?>
+        </div>
+      </header>
+       <!-- <img src="images/about-banner-shadow.png" class="d-block d-sm-none mb-about-shadow" />  -->
+    </section>
+
+    <section class="aboutpageIntro-content">
+      <div class="container container-1070">
+        <h1>Qui sommes-nous</h1>
+        <p>Une équipe de spécialistes à votre service. Nous sommes soucieux de la qualité avec laquelle nous cherchons à
+          répondre à vos besoins. Notre force réside dans une équipe avec des expertises dans des compétences
+          transverses, cette polyvalence nous permet de vous proposer des solutions clé en main et une collaboration sur
+          le long terme. Nous nous sommes engagés à travailler avec des partenaires qui respectent nos valeurs et nos
+          convictions sociales et environnementales.</p>
+      </div>
+    </section>
+
+
+    <section style="margin:auto;" class="aboutpageAlter-content">
+      <div class="row justify-content-center alter-row mt-5">
+        <div class="col-md-4">
+          <div class="card engagement" style="height:100%;">
+            <h2 class="card-title text-center mt-2">Notre engagement environnemental </h2>
+
+            <div class="card-body">
+              <img src="images/engagement-envirennemental.png" class="card-img-top img-fluid mb-2" alt="" />
+              <p class="mt-3">Nous encourageons lorsque c’est possible à<br> réduire la consommation :<br>
+                En incitant le client à utiliser le matériel déjà en sa possession.<br>
+                En concevant des solutions évolutives.<br><br>
+                Nous nous engageons également à réemployer<br> du matériel informatique:<br>
+                En faisant dons aux associations ou aux écoles<br> du matériel considéré obsolète par
+                nos clients,<br>
+                A réparer et/ou réassembler des produits en leur<br> donnant une seconde vie en
+                s’associant avec des partenaires<br> écoresponsables.</p>
+
+            </div>
+          </div>
+        </div>
+
+
+
+        <div class="col-md-4">
+          <div class="card engagement" style="height:100%">
+            <h2 class="card-title text-center mt-2">Notre engagement sociétal</h2>
+
+            <div class="card-body">
+              <img src="images/engagement-societal.png" class="card-img-top img-fluid mb-2 " alt="" />
+              <p class="mt-3">Nous avons inscrit dans l’ADN même de l’entreprise notre responsabilité
+                envers la société.<br><br>
+                L’égalité des chances : le recrutement, la promotion et<br> l’évolution de carrière
+                sera uniquement basée sur les<br> compétences. L’entreprise offre la possibilité aux<br> employés un
+                changement de carrière et l’accès à la formation.<br>
+                La réinsertion professionnelle : nous encourageons les<br> nouveaux départs ou les changements
+                de carrière en<br> dédiant des postes adaptés à cet effet.<br>
+                Culture d’entreprise : nous allons promouvoir au sein de l’entreprise une culture et
+                pratique socialement responsables.</p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section class="notre-approche notre-approche-pc  " style="margin:auto;">
+      <div class="container">
+        <h2 class="mt-5">NOTRE APPROCHE</h2>
+        <div clss="row no-gutters">
+          <div class="col-md-12">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <h3 class="ml-3 ">Empathie</h3>
+                <P class="ml-3">Nous construisons nos solutions par<br> rapport à votre besoin. Dès leurs<br> créations,
+                  elles sont
+                  imaginées pour<br> vous et pour s’inscrire dans le temps<br> afin de créer une réelle coopération<br>
+                  entre
+                  nos équipes.<br>Nos solutions sont imaginées pour<br> vous et pour s’inscrire dans le temps.</P>
+
+              </div>
+              <div class="col-md-2">
+                <img src="images/empathie-img.png" alt="" style="width:80%; height:auto" />
+              </div>
+
+              <div class="col-md-2">
+                <img src="images/surmesure-img.png" alt="" style="width:80%; height:auto" />
+              </div>
+              <div class="col-md-4">
+                <h3 class="ml-4">Sur mesure</h3>
+                <P class="ml-4">La sécurité n'est plus une question de<br>
+                  mode, mais une nécessité pour votre<br> entreprise, c'est pourquoi elle est<br>
+                  imaginée et intégrée dès la conception<br>de chacune de nos propositions.</P>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="row no-gutters mt-5">
+          <div class="col-md-12">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <h3 class="ml-4">Solution adaptable</h3>
+                <P class="ml-4">Votre entreprise est dynamique,elle<br> evolue en permanence et s'adapte aux<br>
+                  changements, c'est pourquoi nos<br>solutions et notre accompagnement<br>
+                  s'adaptent à vous tout au long de votre<br>croissance. </P>
+              </div>
+              <div class="col-md-2">
+                <img src="images/solution-img.png" alt="" style="width:80%; height:auto" />
+              </div>
+              <div class="col-md-2">
+                <img src="images/securite-img.png" alt="" style="width:80%; height:auto" />
+              </div>
+              <div class="col-md-4">
+                <h3 class="ml-4">Sécurité dés la conception</h3>
+                <P class="ml-4">La sécurité n'est plus une question de<br>
+                  mode, mais une nécessité pour votre<br> entreprise, c'est pourquoi elle est<br>
+                  imaginée et intégrée dès la conception<br>de chacune de nos propositions.</P>
+                </P>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Mobile -->
+    <section class="notre-approche notre-approche-mobile" style="margin:auto;">
+      <div class="container">
+        <h2>NOTRE APPROCHE</h2>
+
+        <div class="pos-left">
+          <div class="w-100 mb-3">
+            <img class="d-inline-block mr-2" src="images/empathie-img.png" alt="" style="width:30%; height:auto" />
+            <h3 class="d-inline-block"> Empathie</h3>
+          </div>
+          <p> Nous construisons nos solutions par<br> rapport à votre besoin. Dès leurs<br> créations,
+            elles sont
+            imaginées pour<br> vous et pour s’inscrire dans le temps<br> afin de créer une réelle coopération<br>
+            entre
+            nos équipes.<br>Nos solutions sont imaginées pour<br> vous et pour s’inscrire dans le temps.</P>
+        </div>
+
+
+
+        <div class="pos-right">
+          <div class="w-100 mb-3">
+            <h3 class="d-inline-block mr-2">Sur mesure</h3>
+            <img class="d-inline-block" src="images/surmesure-img.png" alt="" style="width:30%; height:auto"/>
+          </div>
+          <p>La sécurité n'est plus une question de<br>
+            mode, mais une nécessité pour votre entreprise, c'est pourquoi elle est<br>
+            imaginée et intégrée dès la conception<br>de chacune de nos propositions.
+          </p>
+        </div>
+
+
+
+
+
+
+        <div class="pos-left">
+          <div class="w-100 mb-3">
+            <img class="d-inline-block mr-2" src="images/solution-img.png" alt="" style="width:30%; height:auto" />
+            <h3 class="d-inline-block">Solution</h3>
+          </div>
+
+          <P>Votre entreprise est dynamique,elle<br> evolue en permanence et s'adapte aux<br>
+            changements, c'est pourquoi nos<br>solutions et notre accompagnement<br>
+            s'adaptent à vous tout au long de votre<br>croissance. </P>
+        </div>
+
+        <div class="pos-right">
+          <div class="w-100 mb-3">
+            <h3 class="d-inline-block">Sécurité</h3>
+            <img class="d-inline-block mr-2" src="images/securite-img.png" alt="" style="width:30%; height:auto" />
+
+          </div>
+          <P class="mb-4">La sécurité n'est plus une question de<br>
+            mode, mais une nécessité pour votre<br> entreprise, c'est pourquoi elle est<br>
+            imaginée et intégrée dès la conception<br>de chacune de nos propositions.</P>
+          </P>
+
+        </div>
+
+
+
+    </section>
+
+    <?php  include 'includes/highlighted-footer.php'; ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <?php  include 'includes/footer.php'; ?>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/mobile_menu.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
